89fd2c5a4626763734a22f0a25e060fd261434f2
sohal <tosohal@gmail.com>
2023-05-05 16:04:00 +0200

    get the hal from correct repo

--- cmake/cubemx_hal.cmake
+++ cmake/cubemx_hal.cmake
@@ -4,7 +4,7 @@ set(GITHUB_BRANCH_HAL "HEAD" CACHE STRING "git SHA or branch name for HAL")
 cmake_print_variables(GITHUB_BRANCH_HAL)
 FetchContent_Declare(
     hal                             # Recommendation: Stick close to the original name.
-    GIT_REPOSITORY  git@github.com:kodezine/STM32F072RB-DISCO.git
+    GIT_REPOSITORY  git@github.com:kodezine/NUCLEO-STM32G431RB-MC.git
     GIT_TAG         ${GITHUB_BRANCH_HAL}
 )
 
